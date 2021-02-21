a4fd9b0ce4647beddcf737beae26c80f162b0807
Roydon <roystharayil@gmail.com>
2021-02-21 14:13:31 +0530

    add gecodriver

--- 21_02_01.py
+++ 21_02_01.py
@@ -2,3 +2,18 @@
 To scrape Google Trends with keywords
 
 '''
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Firefox(executable_path="C:\\Users\\DELL\\Downloads\\geckodriver\\geckodriver.exe")
+driver.get("https://trends.google.com/trends")
+
+elem = driver.find_element_by_xpath('//*[@id="input-254"]')
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+
+csvIcon = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/md-content/div/div/div[1]/trends-widget/ng-include/widget/div/div/div/widget-actions/div/button[1]')
+
+csvIcon.click()
